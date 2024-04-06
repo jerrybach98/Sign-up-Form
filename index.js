165cdccd09771152fdcad6facf841c0d62e1b24a
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2024-04-06 01:33:06 -0700

    added basic form validation and respective css

---
+++ index.js
@@ -0,0 +1,10 @@
+const password = document.getElementById("password");
+const confirm = document.getElementById("confirm-pass");
+const check = document.getElementById("check");
+
+
+
+const mogu = document.createElement('p');
+mogu.innerHTML = `MOGU: ${computerScore}`;
+score.appendChild(mogu);
+mogu.style.margin = '26px 60px';
